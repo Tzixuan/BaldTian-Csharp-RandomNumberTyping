aa4f81dd1cef464d70156e2a4d47ebf264360bb5
Tzixuan <0171123334@mail.imu.edu.cn>
2019-09-27 11:29:11 +0800

    随机数打字游戏

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace HW0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startbutton = new System.Windows.Forms.Button();
+            this.finishbutton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startbutton
+            // 
+            this.startbutton.Location = new System.Drawing.Point(56, 437);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(100, 33);
+            this.startbutton.TabIndex = 0;
+            this.startbutton.Text = "开始";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.Startbutton_Click);
+            // 
+            // finishbutton
+            // 
+            this.finishbutton.Location = new System.Drawing.Point(219, 438);
+            this.finishbutton.Name = "finishbutton";
+            this.finishbutton.Size = new System.Drawing.Size(99, 32);
+            this.finishbutton.TabIndex = 1;
+            this.finishbutton.Text = "结束";
+            this.finishbutton.UseVisualStyleBackColor = true;
+            this.finishbutton.Click += new System.EventHandler(this.Finishbutton_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(382, 438);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 32);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "退出";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(73, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 80);
+            this.label1.TabIndex = 3;
+            this.label1.Text = " ";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 370);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 25);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(290, 358);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "正确数";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(346, 357);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(290, 390);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "正确率";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(346, 390);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(551, 373);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "速度";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(552, 406);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 31);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(552, 443);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 27);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HW0.Properties.Resources._2;
+            this.pictureBox1.Location = new System.Drawing.Point(383, 310);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 117);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 482);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.finishbutton);
+            this.Controls.Add(this.startbutton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Button finishbutton;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)//窗体标题为当前鼠标坐标
+        {
+            this.Text = e.X + "," + e.Y;
+        }
+
+        private void Startbutton_Click(object sender, EventArgs e)//游戏开始
+        {
+            timer1.Start();
+            this.label1.Text = rnd.Next(1000).ToString();//随机数置初始值
+
+        }
+
+        Random rnd = new Random();//随机数
+        private int correct_count = 0;//正确数
+        private int all_count = 0;//总数
+
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+
+            //label4.Text = label1.Text;
+
+            all_count++;
+            if (textBox1.Text == label1.Text) //记录正确数         
+                label3.Text = (++correct_count).ToString();
+            String count = string.Format("{0,5:P2}", correct_count * 1.0 / all_count);//计算正确率
+            if (all_count != 0)
+            {
+                this.label5.Text = count.ToString();
+            }            
+            this.label1.Location = new Point(rnd.Next(350), rnd.Next(230));//随机数位置随机
+            this.label1.Text = rnd.Next(1000).ToString();//产生随机数
+            this.label1.ForeColor = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));//随机数颜色随机
+            this.textBox1.Text = "";//lable清空
+
+        }
+
+        private void Finishbutton_Click(object sender, EventArgs e)//结束按钮
+        {
+            timer1.Stop();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)//退出按钮
+        {
+            timer1.Stop();
+            DialogResult dr = MessageBox.Show("确定要退出吗？", "提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (dr == DialogResult.OK)
+                Application.Exit();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)//速度变快
+        {
+            timer1.Interval -= 200;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)//速度变慢
+        {
+            timer1.Interval += 200;
+        }
+    }
+}
